3690720272021449c8a704d23657c467342e5602
khalid <khalid-idrissi@hotmail.fr>
2023-07-01 09:10:23 -0400

    update file

--- jenkins.py
+++ jenkins.py
@@ -11,6 +11,13 @@ tpywire = sys.argv[2]
 print(tnetbox)
 print(tpywire)
 
+#pynetbox
+nb = pynetbox.api(
+            url='https://netbox.cbc-rc.ca/',
+            token= tnetbox
+        )
+nb.http_session.verify = False
+
 ####################################################################
 #                      Match device name
 ####################################################################
@@ -59,25 +66,238 @@ def get_token(tokenpywire):
         raise SystemExit(err)
 
 ####################################################################
+#                      update generic devices
+####################################################################
+def update_generic_devices(token):
+
+    roles_bed = list(nb.dcim.device_roles.filter(tag="broadcast-endpoint-devices"))
+    roles_app = list(nb.dcim.device_roles.filter(tag="applications"))
+
+    for role in roles_app:
+        if ':' not in dict(role)['description']:
+            roles_app.remove(role)
+
+    data_role_and_type_none = []
+    data_no_role = []
+    data_no_device_type = []
+    data_updated = []
+    data_type_or_role_none = []
+    data_role_type_exists = []
+    data_alredy_updated = []
+    data_found_not_match = []
+    data_not_found = []
+    data_no_respect_inames = []
+    data_switches = []
+    data_embrionix = []
+    match1_total = []
+    match2_total = []
+    match3_total = []
+    match4_total = []
+
+    devices = nb.dcim.devices.filter(tag='yaml-migration')
+    # devices = nb.dcim.devices.filter(name='MTLMPXCAM008')
+    print(len(devices))
+    regexApp1   = "^[A-Za-z]{3}[A-Za-z]{3}[PNL][PBVC][WLUCEO]([A-Za-z]{3})\d{2}$"  # APPS
+    regexApp2   = "^[A-Za-z]{3}[A-Za-z]{3}[PNL][PBVC]([A-Za-z]{3})\d{3}$"  # APPS
+    regexApp3   = "^[A-Za-z]{3}[A-Za-z]{3}[PNL][PBVC]([A-Za-z]{3})([A-Za-z-0-9]{4})"  # APPS
+    regexBpd    = "^[A-Za-z]{3}[A-Za-z]{3}([A-Za-z]{3})\d{3,}$"  # broadcast endpoint devices
+    regexEmb    = "^[A-Za-z-0-9]+[E|X]LF\d{3}-(\d{2})$"  # embrionix
+    switchregex = r"MTL-\w{4}-[a-zA-Z]{3}-\w+"
+
+    token = get_token()
+    for dev in devices:
+        app1 = re.match(regexApp1, dev.name)
+        app2 = re.match(regexApp2, dev.name)
+        app3 = re.match(regexApp3, dev.name)
+        bed = re.match(regexBpd, dev.name)
+        emb = re.match(regexEmb, dev.name)
+        swt = re.match(switchregex, dev.name)
+
+        result = get_device_data_from_pywire(dev, token)
+        print(result)
+        # if app1:  # Applications type 1
+            # match1_total.append(dev.name)
+            # data = update_device_netbox(dev, result, app1.group(1), roles_app)
+            # data_role_and_type_none.extend(data[0])
+            # data_no_role.extend(data[1])
+            # data_no_device_type.extend(data[2])
+            # data_updated.extend(data[3])
+            # data_found_not_match.extend(data[4])
+            # data_not_found.extend(data[5])
+            # data_alredy_updated.extend(data[6])
+            # data_type_or_role_none.extend(data[7])
+            # data_role_type_exists.extend(data[8])
+
+        # elif app2:  # Applications type 2
+            # match2_total.append(dev.name)
+            # data = update_device_netbox(dev, result, app2.group(1), roles_app)
+            # data_role_and_type_none.extend(data[0])
+            # data_no_role.extend(data[1])
+            # data_no_device_type.extend(data[2])
+            # data_updated.extend(data[3])
+            # data_found_not_match.extend(data[4])
+            # data_not_found.extend(data[5])
+            # data_alredy_updated.extend(data[6])
+            # data_type_or_role_none.extend(data[7])
+            # data_role_type_exists.extend(data[8])
+
+        # elif app3:  # Applications type 3
+            # match3_total.append(dev.name)
+            # data = update_device_netbox(dev, result, app3.group(1), roles_app)
+            # data_role_and_type_none.extend(data[0])
+            # data_no_role.extend(data[1])
+            # data_no_device_type.extend(data[2])
+            # data_updated.extend(data[3])
+            # data_found_not_match.extend(data[4])
+            # data_not_found.extend(data[5])
+            # data_alredy_updated.extend(data[6])
+            # data_type_or_role_none.extend(data[7])
+            # data_role_type_exists.extend(data[8])
+
+        # elif bed:  # Broadcast endpoint devices
+            # match4_total.append(dev.name)
+            # data = update_device_netbox(dev, result, bed.group(1), roles_bed)
+            # data_role_and_type_none.extend(data[0])
+            # data_no_role.extend(data[1])
+            # data_no_device_type.extend(data[2])
+            # data_updated.extend(data[3])
+            # data_found_not_match.extend(data[4])
+            # data_not_found.extend(data[5])
+            # data_alredy_updated.extend(data[6])
+            # data_type_or_role_none.extend(data[7])
+            # data_role_type_exists.extend(data[8])
+
+        # elif emb:  # Embrionix
+            # data_embrionix.append(dev)
+        # #     # update_tag = nb.extras.tags.get(name='yaml_update')
+        # #     # if update_tag not in dev.tags:
+        # #     #     new_tags = [update_tag] + dev.tags
+        # #     #     if int(dev.name.split('-')[1]) % 2 == 0:
+        # #     #         device_type = nb.dcim.device_types.get(slug='eb22hdrt-lm-0516')
+        # #     #     else:
+        # #     #         device_type = nb.dcim.device_types.get(slug='eb22hdrt-lm-0514')
+        # #     #     data = {
+        # #     #         'name': dev.name,
+        # #     #         'site': dev.site.id,
+        # #     #         'device_type': device_type.id,
+        # #     #         'device_role': nb.dcim.device_roles.get(name='Video Gateway').id,
+        # #     #         'tenant': dev.tenant.id,
+        # #     #         'tags': new_tags,
+        # #     #         'status': 'active'
+        # #     #     }
+        # #     #     dev.delete()
+        # #     #     new_device = nb.dcim.devices.create(data)
+        # #     #     if dev:
+        # #     #         print(f'{new_device.name} has been created')
+        # elif swt:
+            # data_switches.append(dev)
+        # else: # devices don't respect inames
+            # data_no_respect_inames.append(dev.name)
+            # data = update_device_netbox(dev, result, dev.name, roles_bed + roles_app)
+            # data_role_and_type_none.extend(data[0])
+            # data_no_role.extend(data[1])
+            # data_no_device_type.extend(data[2])
+            # data_updated.extend(data[3])
+            # data_found_not_match.extend(data[4])
+            # data_not_found.extend(data[5])
+            # data_alredy_updated.extend(data[6])
+            # data_type_or_role_none.extend(data[7])
+            # data_role_type_exists.extend(data[8])
+
+    # # write_to_csv_file(data_role_and_type_none, 'data_role_and_type_none', ['device','type', 'tenant'])
+    # # write_to_csv_file(data_no_device_type, 'data_no_device_type', ['device', 'tenant', 'type', 'role'])
+    # # write_to_csv_file(data_no_role, 'data_no_role', ['device', 'tenant', 'type'])
+
+    # print('------------------------------------------')
+    # print(f'match 1: {len(match1_total)}')
+    # print(match1_total)
+    # print('------------------------------------------')
+    # print(f'match 2: {len(match2_total)}')
+    # print(match2_total)
+    # print('------------------------------------------')
+    # print(f'match 3: {len(match3_total)}')
+    # print(match3_total)
+    # print('------------------------------------------')
+    # print(f'match 4: {len(match4_total)}')
+    # print(match4_total)
+    # print('------------------------------------------')
+    # print(f'data not found: {len(data_not_found)}')
+    # print(data_not_found)
+    # print('------------------------------------------')
+    # print(f'data_role_and_type_none: {len(data_role_and_type_none)}')
+    # print(data_role_and_type_none)
+    # print('------------------------------------------')
+    # print(f'data found not match: {len(data_found_not_match)}')
+    # print(data_found_not_match)
+    # print('------------------------------------------')
+    # print(f'data_updated: {len(data_updated)}')
+    # print(data_updated)
+    # print('------------------------------------------')
+    # print(f'data_no_respect_inames: {len(data_no_respect_inames)}')
+    # print(data_no_respect_inames)
+    # print('------------------------------------------')
+    # print(f'data switches {len(data_switches)}')
+    # print(data_switches)
+    # print('------------------------------------------')
+    # print(f'data_embrionix {len(data_embrionix)}')
+    # print(data_embrionix)
+    # print('------------------------------------------')
+    # print(f'data_no_device_type {len(data_no_device_type)}')
+    # print(data_no_device_type)
+    # print('------------------------------------------')
+    # print(f'data_no_role {len(data_no_role)}')
+    # print(data_no_role)
+    # print('------------------------------------------')
+    # print(f'data_alredy_updated {len(data_alredy_updated)}')
+    # print(data_alredy_updated)
+    # print('------------------------------------------')
+    # print(f'data_type_or_role_none {len(data_type_or_role_none)}')
+    # print(data_type_or_role_none)
+    # print('------------------------------------------')
+    # print(f'data_role_type_exists {len(data_role_type_exists)}')
+    # print(data_role_type_exists)
+    # print('------------------------------------------')
+
+    # type_counts = {}
+    # for item in data_no_device_type:
+        # device_type = item['type']
+        # if device_type in type_counts:
+            # type_counts[device_type] += 1
+        # else:
+            # type_counts[device_type] = 1
+
+    # type_counts_2 = {}
+    # for item in data_role_and_type_none:
+        # device_type = item['type']
+        # if device_type in type_counts_2:
+            # type_counts_2[device_type] += 1
+        # else:
+            # type_counts_2[device_type] = 1
+    # merged_dict = {**type_counts, **type_counts_2}
+    # sorted_items = sorted(merged_dict.items(), key=lambda x: x[1], reverse=True)
+    # top_10_items = sorted_items[:10]
+    # total_devices = len(devices)
+    # total_updated = nb.dcim.devices.filter(tag='yaml_update')
+    # ip_pres_updated = []
+    # ip_prod_updated = []
+    # for dev in total_updated:
+        # if dev.tenant.name == 'Presentation':
+            # ip_pres_updated.append(dev)
+        # elif dev.tenant.name == 'Media Production':
+            # ip_prod_updated.append(dev)
+    # total_applications = len(match1_total) + len(match2_total) + len(match3_total)
+    # update_conflence_page(total_devices, len(total_updated), len(data_embrionix), len(data_not_found), len(data_role_and_type_none), len(data_no_device_type), len(data_no_role), len(data_found_not_match),top_10_items, len(ip_pres_updated), len(ip_prod_updated), total_applications, len(data_switches), len(match4_total), len(data_updated), len(data_role_type_exists), len(data_no_respect_inames))
+
+
+
+####################################################################
 #                     Main
 ####################################################################
 
 if __name__ == '__main__':
 
-    nb = pynetbox.api(
-            url='https://netbox.cbc-rc.ca/',
-            token= tnetbox
-        )
-    nb.http_session.verify = False
-    device = nb.dcim.devices.get(name='MTLMPXITC2004')
-    print(device.id)
-    print('end of the programm')
-    
     token_result = get_token(tpywire)
-    result = find_switch("MTLPREPPIMAW004", token_result)
-    roles_bed = list(nb.dcim.device_roles.filter(tag="broadcast-endpoint-devices"))
-    roles_app = list(nb.dcim.device_roles.filter(tag="applications"))
-    print(roles_bed)
-    print(roles_app)
+    update_generic_devices(token_result)
+    
     
     
