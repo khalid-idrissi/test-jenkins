95b922a3a2471ede73223bca2873c56df5d6cac5
khalid <khalid-idrissi@hotmail.fr>
2023-07-02 14:07:43 -0400

    update file

--- jenkins.py
+++ jenkins.py
@@ -8,15 +8,10 @@ from slugify import slugify
 from atlassian import Confluence
 
 urllib3.disable_warnings()
-
 # Access environment variables
 tnetbox = sys.argv[1]
 tpywire = sys.argv[2]
 atlassiantoken = sys.argv[3]
-
-#print parameters
-print(tnetbox)
-print(tpywire)
 
 #pynetbox
 nb = pynetbox.api(
@@ -24,7 +19,6 @@ nb = pynetbox.api(
             token= tnetbox
         )
 nb.http_session.verify = False
-
 
 ####################################################################
 #                 Trace path by switch and port
@@ -143,33 +137,7 @@ def get_device_data_from_pywire(dev, token):
                         return result
 
             return result
-####################################################################
-#                      Match device name
-####################################################################
-def find_switch(device_name, token):
-    url = 'https://pywire-app.cbc-rc.ca/api/v2/template_data_from_device_with_auto_match/?device_name=' + device_name
 
-    headers = {
-        'Accept': 'application/json',
-        'X-Access-Token': token
-    }
-    response = req.request("GET", url, headers=headers)
-    if response.status_code == 200:
-        responseData = response.json()
-        print(responseData)
-        if responseData['result'] == 'no match':
-            return None
-        else:
-            if responseData['result'].split('-')[0] == device_name.split('-')[0] and responseData['result'].split('-')[
-               2] == device_name.split('-')[2] and responseData['result'].split('-')[3] == device_name.split('-')[3]:
-               
-               print(responseData['result'])
-               return responseData['result']
-            else:
-               return None
-
-    else:
-        print(str(response.status_code) + ' : ' + response.text + str(device_name))
 ####################################################################
 #                      Get Token
 ####################################################################
